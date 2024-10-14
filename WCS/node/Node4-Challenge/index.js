1ae990c1a5b09ab28bdab21247cae7b7dbb40842
RomainVarra <romain.varra@hotmail.com>
2024-10-14 17:38:22 +0200

    Update index.js

--- WCS/node/Node4-Challenge/index.js
+++ WCS/node/Node4-Challenge/index.js
@@ -1,2 +1,5 @@
 import "dotenv/config";
+const MY_NAME = process.env.MY_NAME;
+const MY_CITY = process.env.MY_CITY;
+const MY_LANGUAGE = process.env.MY_LANGUAGE;
 console.log(`I am ${process.env.MY_NAME}, wilder in${process.env.MY_CITY}, and I love ${process.env.MY_LANGUAGE}`);
