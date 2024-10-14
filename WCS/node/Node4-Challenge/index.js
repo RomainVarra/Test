1d5ea61d470397d7e00c7c92e85961e442a61d78
Romain Varra <romain.varra@hotmail.com>
2024-10-14 12:44:04 +0200

    FEAT: creation of .gitignore and .env.sample

---
+++ WCS/node/Node4-Challenge/index.js
@@ -0,0 +1,4 @@
+import "dotenv/config";
+console.log(`My city is ${process.env.MY_CITY}`);
+console.log(`My name is ${process.env.MY_NAME}`);
+console.log(`My language is ${process.env.MY_LANGUAGE}`);
